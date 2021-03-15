30cec0018eba12eb7e49e988f99b478bdc17a227
Kevin Hans Christiono <kevin.christiono@gmail.com>
2021-03-15 12:11:43 +0700

    Create project kelompok 2

---
+++ Kelompok 2/projectkelompok2/src/main/java/com/blibli/projectkelompok2/Projectkelompok2Application.java
@@ -0,0 +1,13 @@
+package com.blibli.projectkelompok2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectkelompok2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projectkelompok2Application.class, args);
+	}
+
+}
---
+++ Kelompok 2/projectkelompok2/src/test/java/com/blibli/projectkelompok2/Projectkelompok2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blibli.projectkelompok2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectkelompok2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
